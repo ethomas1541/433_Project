105624f43ccc82f72baab314adcdaf2d58d31cf5
Elijah <ethomas1541@gmail.com>
2024-03-14 20:41:30 -0700

    Added router IPs to hopdump.txt during every traceroute

--- hops.sh
+++ hops.sh
@@ -1,7 +1,9 @@
-printf "HOPS TO $1\n\nTIME\t\tHOPS\n" > hopdump.txt
+printf "HOPS TO $1\n\nTIME\t\tHOPS\tROUTER IPs\n" > hopdump.txt
 
 while true
 do
-    printf "$(date '+%H:%M:%S')\t" >> hopdump.txt
-    expr $(traceroute $1 | wc -l) - 1 >> hopdump.txt
+    printf "\n$(date '+%H:%M:%S')\t" >> hopdump.txt
+    traceroute $1 > rt.txt
+    printf "$(expr $(cat rt.txt | wc -l) - 1)\t\t" >> hopdump.txt
+    cat rt.txt | grep -Po "^(?:.*?)\((?:\d{1,3}\.){3}\d{1,3}" | grep -Po "(?<=\().*" | tail -n +2 | tr '\n' ' ' >> hopdump.txt
 done
