e61ad762c652e07b2f95487cb9d1a5467d23e0e5
Elijah <ethomas1541@gmail.com>
2024-03-17 18:00:52 -0700

    Limited runtime

--- hops.sh
+++ hops.sh
@@ -2,8 +2,7 @@ printf "HOPS TO $1\n\nTIME\t\tHOPS\tROUTER IPs\n" > hopdump.txt
 
 while true
 do
-    printf "\n$(date '+%H:%M:%S')\t" >> hopdump.txt
     traceroute $1 > rt.txt
-    printf "$(expr $(cat rt.txt | wc -l) - 1)\t\t" >> hopdump.txt
-    cat rt.txt | grep -Po "^(?:.*?)\((?:\d{1,3}\.){3}\d{1,3}" | grep -Po "(?<=\().*" | tail -n +2 | tr '\n' ' ' >> hopdump.txt
+    printf "\n$(date '+%H:%M:%S')\t$(expr $(cat rt.txt | wc -l) - 1)\t\t" >> hopdump.txt
+    cat rt.txt | grep -Po "^(?:.*?)\((?:\d{1,3}\.){3}\d{1,3}" | grep -Po "(?<=\().*" | tail -n +2 | tr '\n' ' '>> hopdump.txt
 done
